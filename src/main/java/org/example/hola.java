1f99254c1c995eea6c95679d2aff825f660e5dc1
sil-graci <silviagraci@gmail.com>
2023-03-23 07:14:24 -0300

    lala

--- src/main/java/org/example/hola.java
+++ src/main/java/org/example/hola.java
@@ -6,6 +6,8 @@ public static void main(String[] args){
     System.out.println("chau");
     System.out.println("chau");
     System.out.println("chau");
+    System.out.println("chau");
+    System.out.println("chau");
 }
 
 
