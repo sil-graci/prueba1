5a9aa086d15e7554cda0adb28964ba25c26811d9
sil-graci <silviagraci@gmail.com>
2023-03-23 07:33:50 -0300

    jjjj

--- src/main/java/org/example/hola.java
+++ src/main/java/org/example/hola.java
@@ -4,10 +4,7 @@ public class hola {
 public static void main(String[] args){
     System.out.println("hola");
     System.out.println("chau");
-    System.out.println("chau");
-    System.out.println("chau");
-    System.out.println("chau");
-    System.out.println("chau");
+ 
 }
 
 
